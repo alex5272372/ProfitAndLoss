cb1ab245d5542ff243aa80e6fcd35246d2a55bc6
alex5272372 <alex5272372@gmail.com>
2020-09-11 15:20:28 +0300

    Fill tables for DE

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Profit & Loss
+## Project Deployment
+
+git clone https://github.com/alex5272372/ProfitAndLoss.git
+npm install
+npx ubcli createStore
+npx ubcli initDB -p admin -dba postgres -dbaPwd FS9final -drop -create -host http://localhost:8881
+npx ubcli generateDDL -u admin -p admin -host http://localhost:8881 -autorun
+npx ubcli initialize -u admin -p admin -host http://localhost:8881
---
+++ models/DE/public/forms/de_ruleEntity-fm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="u-form-layout">
+    <u-toolbar />
+    <u-form-container
+      v-loading.body="loading"
+    >
+      <u-auto-field attribute-name="parentRuleModel" />
+      <u-auto-field attribute-name="name" />
+      <u-auto-field attribute-name="entitySource" />
+      <u-auto-field attribute-name="entityDestination" />
+    </u-form-container>
+  </div>
+</template>
+
+<script>
+const { Form } = require('@unitybase/adminui-vue')
+const { mapGetters } = require('vuex')
+
+module.exports.mount = cfg => {
+  Form(cfg)
+    .processing()
+    .validation()
+    .mount()
+}
+
+module.exports.default = {
+  name: 'ReqSubDepart',
+  computed: {
+    ...mapGetters(['loading'])
+  }
+}
+</script>
